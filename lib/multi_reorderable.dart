6e94a219921c9ae84a01a2e52fcf88a87f160faf
Abdelrahman Atef <abduu0000@yahoo.com>
2025-04-04 02:21:00 +0200

    Generate index exports

--- lib/multi_reorderable.dart
+++ lib/multi_reorderable.dart
@@ -1,1 +1,10 @@
-library multi_reorderable;
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+library;
+
+export 'reorderable_multi_select.dart';
+export 'reorderable_multi_select_list.dart';
+export 'src/builders/builders.dart';
+export 'src/models/reorderable_item_model.dart';
+export 'src/reorderable_multi_select_list.dart';
+export 'src/theme/reorderable_multi_select_theme.dart';
